95494308a2a9ad350a811ccdc2ab9dc3d729ed39
Rishi Srivastava <rishisrivasta@gmail.com>
2021-06-29 06:37:33 +0800

    use singleton and annotations with Provides

--- simple/src/main/java/org/rishi/module/AppModule.java
+++ simple/src/main/java/org/rishi/module/AppModule.java
@@ -2,6 +2,9 @@ package org.rishi.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import org.rishi.annotations.ColorValue;
+import org.rishi.annotations.EdgeSize;
 import org.rishi.service.DrawShape;
 import org.rishi.service.DrawSquare;
 
@@ -13,11 +16,14 @@ public class AppModule extends AbstractModule {
     @Override
     protected void configure() {
         // bind(DrawShape.class).to(DrawSquare.class);
+        bind(String.class).annotatedWith(ColorValue.class).toInstance("Red");
+        bind(Integer.class).annotatedWith(EdgeSize.class).toInstance(40);
     }
 
     @Provides
-    DrawShape providesDrawSquare() {
-        DrawShape d = new DrawSquare("Red", 40);
+    @Singleton
+    DrawShape providesDrawSquare(@ColorValue String color, @EdgeSize Integer edge) {
+        DrawShape d = new DrawSquare(color, edge);
         return d;
     }
 }
